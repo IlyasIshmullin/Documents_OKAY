418177bedd25eb0713f99382e3b872858253fd5d
migranchik <gulsiya2668@gmail.com>
2022-04-15 23:50:03 +0500

    Added  activity_authorization.xml,  AuthorizationActivity.java  User.java Changed res files  colors.xml  strings.xml

---
+++ app/src/main/java/com/example/documents_okay/User.java
@@ -0,0 +1,12 @@
+package com.example.documents_okay;
+
+public class User {
+    public String name;
+    public String surname;
+    public String email;
+    public User(String name, String surname, String email){
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+}
